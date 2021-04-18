c50f27130b90d5ad487fdb4c7a351b067aee7b47
strangeCamel <voice@void>
2021-04-18 14:38:55 +0300

    command line interface improvements

--- src/strange.cpp
+++ src/strange.cpp
@@ -27,235 +27,348 @@ static bool TrimLine(std::string &line)
 
 struct LoadLines : std::vector<std::string>
 {
-	LoadLines(const char *file)
+	template <class IStream>
+		bool Load(IStream &is)
 	{
-		std::ifstream infile(file);
 		std::string line;
-		while (std::getline(infile, line)) if (TrimLine(line)) {
+		while (std::getline(is, line)) if (TrimLine(line)) {
 			emplace_back(line);
 		}
+		return true;
 	}
 };
 
-static void PrintMatchingLine(bool color, const std::string &line)
+class Commander
 {
-	if (color) {
-		std::cout << "\033[32m" << line << "\033[m" << std::endl;
+	AutoPatternsC::TriePtr _t;
+	size_t _context = 0;
+	int _exit_code = 0;
+	bool _descript = false;
+	bool _color = false;
 
-	} else {
-		std::cout << ' ' << line << std::endl;
+	enum ExitCodeBit
+	{
+		ECB_ANOMALY = 0x01,
+		ECB_READ_ERROR = 0x02,
+		ECB_WRITE_ERROR = 0x04,
+		ECB_CMDLINE_ERROR = 0x20,
+		ECB_UNSPECIFIED_ERROR = 0x40,
+	};
+
+	void ToggleExitCode(ExitCodeBit bit)
+	{
+		_exit_code|= bit;
 	}
-}
 
-static void PrintMismatchingLine(AutoPatternsC::TriePtr &t, bool descript, bool color, const std::string &line)
-{
-	if (!color) {
-		std::cout << '!';
-	}
-
-	if (descript) {
-		const auto &sd = t->Descript(line);
-		char status_fin_char = -1;
-		for (const auto &td : sd) {
-			switch (td.status) {
-				case AutoPatternsC::TS_MATCH:
-					if (color && status_fin_char) {
-						std::cout << "\033[1;32m";
-					} else if (status_fin_char > 0) {
-						std::cout << status_fin_char;
-					}
-					status_fin_char = 0;
-					break;
-				case AutoPatternsC::TS_MISMATCH:
-					if (color && status_fin_char != ']') {
-						std::cout << "\033[1;33m";
-					} else if (status_fin_char != ']') {
-						if (status_fin_char > 0) {
-							std::cout << status_fin_char;
-						}
-						std::cout << '[';
-					}
-					status_fin_char = ']';
-					break;
-				case AutoPatternsC::TS_REDUNDANT:
-					if (color && status_fin_char != '>') {
-						std::cout << "\033[1;31m";
-					} else if (status_fin_char != '>') {
-						if (status_fin_char > 0) {
-							std::cout << status_fin_char;
-						}
-						std::cout << '<';
-					}
-					status_fin_char = '>';
-					break;
-				case AutoPatternsC::TS_MISSING:
-					if (color && status_fin_char != ')') {
-						std::cout << "\033[1;31m";
-					} else if (status_fin_char != ')') {
-						if (status_fin_char > 0) {
-							std::cout << status_fin_char;
-						}
-						std::cout << '(';
-					}
-					status_fin_char = ')';
-					break;
-			}
-			if (td.status != AutoPatternsC::TS_MISSING) {
-				std::cout << td.token;
-			} else {
-				std::cout << "\xE2\x80\xA2"; // '?';//
-			}
-		}
-		if (color) {
-			std::cout << "\033[m";
-
-		} else if (status_fin_char > 0) {
-			std::cout << status_fin_char;
+	bool CheckOperandsCount(const std::string &cmd, int expected_count, int operands_count)
+	{
+		if (expected_count == operands_count) {
+			return true;
 		}
 
-
-	} else if (color) {
-		std::cout << "\033[1;33m" << line << "\033[m";
-
-	} else {
-		std::cout << line;
+		ToggleExitCode(ECB_CMDLINE_ERROR);
+		std::cerr << "Wrong operands count for " << cmd << ": " << std::dec << operands_count << std::endl;
+		return false;
 	}
 
-	std::cout << std::endl;
-}
+	////////
 
-template <class IStream>
-	static void EvalStream(AutoPatternsC::TriePtr &t, IStream &is, size_t context, bool descript, bool color)
-{
-	std::string line;
-	std::list<std::string> context_matching_backlog;
-	size_t context_matching_countdown = 0;
-	for (size_t index = 0; std::getline(is, line); ++index) if (TrimLine(line)) {
-		if (!t->Match(line)) {
-			if (context != 0 && context != std::string::npos) {
-				context_matching_countdown = context;
-				if (!context_matching_backlog.empty()) {
-					std::cout << std::endl;
-					for (const auto &matching_line : context_matching_backlog) {
-						PrintMatchingLine(color, matching_line);
-					}
+	void PrintMatchingLine(const std::string &line)
+	{
+		if (_color) {
+			std::cout << "\033[32m" << line << "\033[m" << std::endl;
+		} else {
+			std::cout << ' ' << line << std::endl;
+		}
+	}
+
+	void PrintMismatchingLine(const std::string &line)
+	{
+		if (!_color) {
+			std::cout << '!';
+		}
+
+		if (_descript) {
+			const auto &sd = _t->Descript(line);
+			char status_fin_char = -1;
+			for (const auto &td : sd) {
+				switch (td.status) {
+					case AutoPatternsC::TS_MATCH:
+						if (_color && status_fin_char) {
+							std::cout << "\033[1;32m";
+						} else if (status_fin_char > 0) {
+							std::cout << status_fin_char;
+						}
+						status_fin_char = 0;
+						break;
+					case AutoPatternsC::TS_MISMATCH:
+						if (_color && status_fin_char != ']') {
+							std::cout << "\033[1;33m";
+						} else if (status_fin_char != ']') {
+							if (status_fin_char > 0) {
+								std::cout << status_fin_char;
+							}
+							std::cout << '[';
+						}
+						status_fin_char = ']';
+						break;
+					case AutoPatternsC::TS_REDUNDANT:
+						if (_color && status_fin_char != '>') {
+							std::cout << "\033[1;31m";
+						} else if (status_fin_char != '>') {
+							if (status_fin_char > 0) {
+								std::cout << status_fin_char;
+							}
+							std::cout << '<';
+						}
+						status_fin_char = '>';
+						break;
+					case AutoPatternsC::TS_MISSING:
+						if (_color && status_fin_char != ')') {
+							std::cout << "\033[1;31m";
+						} else if (status_fin_char != ')') {
+							if (status_fin_char > 0) {
+								std::cout << status_fin_char;
+							}
+							std::cout << '(';
+						}
+						status_fin_char = ')';
+						break;
+				}
+				if (td.status != AutoPatternsC::TS_MISSING) {
+					std::cout << td.token;
+				} else {
+					std::cout << "\xE2\x80\xA2"; // '?';//
 				}
 			}
-			PrintMismatchingLine(t, descript, color, line);
-			context_matching_backlog.clear();
+			if (_color) {
+				std::cout << "\033[m";
+			} else if (status_fin_char > 0) {
+				std::cout << status_fin_char;
+			}
 
-		} else if (context == 0) {
-			;
 
-		} else if (context == std::string::npos) {
-			PrintMatchingLine(color, line);
-
-		} else if (context_matching_countdown) {
-			PrintMatchingLine(color, line);
-			--context_matching_countdown;
+		} else if (_color) {
+			std::cout << "\033[1;33m" << line << "\033[m";
 
 		} else {
-			context_matching_backlog.emplace_back(line);
-			if (context_matching_backlog.size() > context) {
-				context_matching_backlog.pop_front();
+			std::cout << line;
+		}
+
+		std::cout << std::endl;
+	}
+
+	template <class IStream>
+		void EvalStream(IStream &is)
+	{
+		std::string line;
+		std::list<std::string> context_matching_backlog;
+		size_t context_matching_countdown = 0;
+		for (size_t index = 0; std::getline(is, line); ++index) if (TrimLine(line)) {
+			if (!_t->Match(line)) {
+				ToggleExitCode(ECB_ANOMALY);
+				if (_context != 0 && _context != std::string::npos) {
+					context_matching_countdown = _context;
+					if (!context_matching_backlog.empty()) {
+						for (const auto &matching_line : context_matching_backlog) {
+							PrintMatchingLine(matching_line);
+						}
+					}
+				}
+				PrintMismatchingLine(line);
+				context_matching_backlog.clear();
+
+			} else if (_context == 0) {
+				;
+
+			} else if (_context == std::string::npos) {
+				PrintMatchingLine(line);
+
+			} else if (context_matching_countdown) {
+				PrintMatchingLine(line);
+				--context_matching_countdown;
+				if (context_matching_countdown == 0) {
+					std::cout << std::endl;
+				}
+
+			} else {
+				context_matching_backlog.emplace_back(line);
+				if (context_matching_backlog.size() > _context) {
+					context_matching_backlog.pop_front();
+				}
 			}
 		}
 	}
-}
 
-int main(int argc, char **argv)
-{
-	AutoPatternsC::TriePtr t;
-	int out = 0;
-	size_t context = 0;
-	bool descript = false, color = false;
-	for (int i = 1; i < argc; ++i) {
-		const char *argvi = argv[i];
-		while (argvi[0] == '-' && argvi[1] == '-') {
-			++argvi;
-		}
 
-		if (strcmp(argvi, "-descript") == 0) {
-			descript = true;
+	void ExecuteInner(const std::string &cmd, char **operands, int operands_count)
+	{
+		if (cmd == "descript") {
+			_descript = true;
+			CheckOperandsCount(cmd, 0, operands_count);
 
-		} else if (strcmp(argvi, "-color") == 0) {
-			color = true;
+		} else if (cmd == "color") {
+			_color = true;
+			CheckOperandsCount(cmd, 0, operands_count);
 
-		} else if (strstr(argvi, "-context=") == argvi) {
-			const char *val = strchr(argvi, '=') + 1;
-			context = strcasecmp(val, "ALL") ? atoi(val) : (size_t)-1;
+		} else if (cmd == "context") {
+			if (operands_count == 0) {
+				_context = 3;
 
-		} else if (strcmp(argvi, "-eval") == 0) {
-			if (!t) {
-				std::cerr << "ERROR: No trie for eval" << std::endl;
-				out = -1;
-				continue;
+			} else if (CheckOperandsCount(cmd, 1, operands_count)) {
+				_context = strcasecmp(*operands, "ALL") ? atoi(*operands) : (size_t)-1;
 			}
 
-			EvalStream(t, std::cin, context, descript, color);
+		} else if (cmd == "eval") {
+			if (!_t) {
+				ToggleExitCode(ECB_CMDLINE_ERROR);
+				std::cerr << "No trie for " << cmd << std::endl;
 
-		} else if (strstr(argvi, "-eval=") == argvi) {
-			if (!t) {
-				std::cerr << "ERROR: No trie for eval" << std::endl;
-				out = -1;
-				continue;
+			} else if (operands_count == 0) {
+				EvalStream(std::cin);
+
+			} else for (int i = 0; i < operands_count; ++i) {
+				std::ifstream is(operands[i]);
+				if (!is.is_open()) {
+					ToggleExitCode(ECB_READ_ERROR);
+					std::cerr << "Can't open: " << operands[i] << std::endl;
+				} else {
+					EvalStream(is);
+				}
 			}
-			std::ifstream infile(strchr(argvi, '=') + 1);
-			EvalStream(t, infile, context, descript, color);
 
-		} else if (strstr(argvi, "-learn=") == argvi) {
-			LoadLines lines(strchr(argvi, '=') + 1);
-			if (!t) {
-				t.reset(new AutoPatternsC::Trie);
+		} else if (cmd == "learn") {
+			if (!_t) {
+				_t.reset(new AutoPatternsC::Trie);
 			}
-			t->Learn(lines);
+			LoadLines lines;
+			if (operands_count == 0) {
+				lines.Load(std::cin);
 
-		} else if (strstr(argvi, "-load=") == argvi) {
-			const char *fpath = strchr(argvi, '=') + 1;
-			std::ifstream is(fpath);
-			if (t) {
+			} else for (int i = 0; i < operands_count; ++i) {
+				std::ifstream is(operands[i]);
+				if (!is.is_open()) {
+					ToggleExitCode(ECB_READ_ERROR);
+					std::cerr << "Can't open: " << operands[i] << std::endl;
+				} else {
+					lines.Load(is);
+				}
+			}
+			_t->Learn(lines);
+
+		} else if (cmd == "load") {
+			if (_t) {
 				std::cerr << "WARNING: Load dismisses previous trie" << std::endl;
 			}
-			try {
-				t.reset(new AutoPatternsC::Trie(is));
-			} catch (std::exception &e) {
-				std::cerr << e.what() << " while loading " << fpath << std::endl;
+			if (operands_count == 0) {
+				_t.reset(new AutoPatternsC::Trie(std::cin));
+
+			} else {
+				CheckOperandsCount(cmd, 1, operands_count);
+				std::ifstream is(operands[0]);
+				if (!is.is_open()) {
+					ToggleExitCode(ECB_READ_ERROR);
+					std::cerr << "Can't open: " << operands[0] << std::endl;
+				} else {
+					_t.reset(new AutoPatternsC::Trie(is));
+				}
 			}
 
-		} else if (strstr(argvi, "-save=") == argvi
-				|| strstr(argvi, "-save-compact=") == argvi) {
-			if (!t) {
-				std::cerr << "ERROR: No trie for save" << std::endl;
-				continue;
+		} else if (cmd == "save" || cmd == "save-compact") {
+			if (!_t) {
+				ToggleExitCode(ECB_CMDLINE_ERROR);
+				std::cerr << "No trie for " << cmd << std::endl;
+
+			} else if (operands_count == 0) {
+				_t->Save(std::cout, cmd == "save-compact");
+
+			} else for (int i = 0; i < operands_count; ++i) {
+				std::ofstream os(operands[i]);
+				if (!os.is_open()) {
+					ToggleExitCode(ECB_WRITE_ERROR);
+					std::cerr << "Can't create:" << operands[i] << std::endl;
+				} else {
+					_t->Save(os, cmd == "save-compact");
+				}
 			}
 
-			std::ofstream os(strchr(argvi, '=') + 1);
-			t->Save(os, strstr(argvi, "-save-compact=") == argvi);
-
-		} else if (*argvi) {
-			if (strstr(argvi, "-help") != argvi) {
-				std::cerr << "Bad argument: " << argvi << std::endl;
+		} else {
+			if (cmd != "help") {
+				std::cerr << "Bad argument: " << cmd << std::endl;
+				ToggleExitCode(ECB_CMDLINE_ERROR);
 			}
 			std::cerr << "Strange Tool by strangeCamel, BETA " << VERINFO << std::endl;
-			std::cerr << "Usage: " << argv[0]
-				<< " [-load=TRIE_FILE] [-learn=SAMPLES_FILE] [-descript] [-color] [-context=#] [-eval=SAMPLES_FILE] [-eval] [-save=TRIE_FILE] [-save-compact=TRIE_FILE]"
+			std::cerr << "Usage: strange"
+				<< " [-load TRIE_FILE] [-learn SAMPLES_FILE1 [SAMPLES_FILE2..]] [-descript] [-color] [-context [#]] [-eval SAMPLES_FILE1 [SAMPLES_FILE2..]] [-save TRIE_FILE] [-save-compact TRIE_FILE]"
 					<< std::endl;
 			std::cerr << "Operations are executed in exactly same order as specified by command line." << std::endl;
 			std::cerr << "Operations description:" << std::endl;
-			std::cerr << "  -load= loads ready to use patterns from specified trie file. Loading discards any already existing in memory patterns (from previous load or learn operations)." << std::endl;
-			std::cerr << "  -learn= learns samples from specified text file. If there're some already existing patterns in memory - learning will incrementally extend them, without discarding." << std::endl;
+			std::cerr << "  -load loads ready to use patterns from specified trie file. Loading discards any already existing in memory patterns (from previous load or learn operations)." << std::endl;
+			std::cerr << "  -learn learns samples from specified text file(s) or stdin if no files specified. If there're some already existing patterns in memory - learning will incrementally extend them, without discarding." << std::endl;
 			std::cerr << "  -descript enables per-token description of anomal lines found by -eval operation (can be slow)." << std::endl;
 			std::cerr << "  -color enables using of ASCII colors in output of -eval operation." << std::endl;
-			std::cerr << "  -context= makes -eval operation to print # number of lines before and after each mismatched line. If # is ALL then everything will be printed." << std::endl;
-			std::cerr << "  -eval= evaluates samples from specified text file and prints to stdout results" << std::endl;
-			std::cerr << "  -eval evaluates samples written to standard input, behaves same as --eval=..." << std::endl;
-			std::cerr << "  -save= saves existing in memory patterns into specified trie file with indentation for better readablity." << std::endl;
-			std::cerr << "  -save-compact= saves existing in memory patterns into specified trie file in compact form to save space." << std::endl;
-			out = -2;
-			break;
+			std::cerr << "  -context makes -eval operation to print # number of lines before and after each mismatched line. If # is ALL then everything will be printed. If # is omitted - then its defaulted to 3 lines." << std::endl;
+			std::cerr << "  -eval evaluates samples from specified text file(s) and prints results to stdout." << std::endl;
+			std::cerr << "  -save saves existing in memory patterns into specified trie file with indentation for better readablity." << std::endl;
+			std::cerr << "  -save-compact saves existing in memory patterns into specified trie file in compact form to save space." << std::endl;
+			std::cerr << "Exit code composed of following bits:" << std::endl;
+			std::cerr << std::dec;
+			std::cerr << "  " << ECB_ANOMALY << " if -eval used and found anomalies" << std::endl;
+			std::cerr << "  " << ECB_READ_ERROR << " if failed to read some files" << std::endl;
+			std::cerr << "  " << ECB_WRITE_ERROR << " if failed to write some files" << std::endl;
+			std::cerr << "  " << ECB_CMDLINE_ERROR << " in case of bad command line arguments" << std::endl;
+			std::cerr << "  " << ECB_UNSPECIFIED_ERROR << " in case of any other failure" << std::endl;
 		}
 	}
 
-//for (;;);
-	return out;
+public:
+	void Execute(const std::string &cmd, char **operands, int operands_count)
+	{
+		try {
+			ExecuteInner(cmd, operands, operands_count);
+
+		} catch (std::exception &e) {
+			ToggleExitCode(ECB_UNSPECIFIED_ERROR);
+			std::cerr << "Error in '" << cmd << "': " << e.what() << std::endl;
+		}
+	}
+
+	int ExitCode() const
+	{
+		return _exit_code;
+	}
+};
+
+int main(int argc, char **argv)
+{
+	Commander c;
+	int last_arg_cmd = 0;
+	std::string cmd;
+	if (argc <= 1) {
+		c.Execute("help", nullptr, 0);
+
+	} else for (int i = 1; i <= argc; ++i) {
+		const char *argvi = argv[i];
+		if (i >= argc || argv[i][0] == '-') {
+			if (last_arg_cmd != 0) {
+				char *arg_cmd = argv[last_arg_cmd];
+				while (*arg_cmd == '-') {
+					++arg_cmd;
+				}
+				char *eq = strchr(arg_cmd, '=');
+				if (eq) {
+					cmd.assign(arg_cmd, eq - arg_cmd);
+					argv[last_arg_cmd] = eq + 1;
+					c.Execute(cmd, &argv[last_arg_cmd], i - last_arg_cmd);
+				} else {
+					cmd.assign(arg_cmd);
+					c.Execute(cmd, &argv[last_arg_cmd + 1], i - (last_arg_cmd + 1));
+				}
+
+			}
+			last_arg_cmd = i;
+		}
+	}
+
+	return c.ExitCode();
 }
